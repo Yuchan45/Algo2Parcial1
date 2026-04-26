b11a331578025bb01c51a903034f8a0a9cc9943c
Yu Nakasone <yu.nakasone@gmail.com>
2026-04-26 16:29:44 -0300

    Implemented Queue. Dynamic and static

--- algorithms-II/src/main/java/org/uade/structure/implementation/dynamic/DynamicQueueADT.java
+++ algorithms-II/src/main/java/org/uade/structure/implementation/dynamic/DynamicQueueADT.java
@@ -1,29 +1,62 @@
 package org.uade.structure.implementation.dynamic;
 
 
+import org.uade.exception.EmptyADTException;
 import org.uade.structure.definition.QueueADT;
 
 // Esta clase representa la implementacion dinamica del TDA Cola.
 public class DynamicQueueADT implements QueueADT {
 
+    private Node first;
+    private Node last;
 
     @Override
     public int getElement() {
-        return 0;
+        if (this.isEmpty()) {
+            throw new EmptyADTException();
+        }
+
+        return this.first.value;
     }
 
     @Override
     public void add(int value) {
+        Node newNode = new Node(value);
 
+        if (this.isEmpty()) {
+            this.first = newNode;
+            this.last = newNode;
+            return;
+        }
+
+        this.last.next = newNode;
+        this.last = newNode;
     }
 
     @Override
     public void remove() {
+        if (this.isEmpty()) {
+            throw new EmptyADTException();
+        }
 
+        this.first = this.first.next;
+
+        if (this.first == null) {
+            this.last = null;
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.first == null;
+    }
+
+    private static class Node {
+        private final int value;
+        private Node next;
+
+        private Node(int value) {
+            this.value = value;
+        }
     }
 }
--- algorithms-II/src/main/java/org/uade/structure/implementation/fixed/StaticQueueADT.java
+++ algorithms-II/src/main/java/org/uade/structure/implementation/fixed/StaticQueueADT.java
@@ -1,26 +1,57 @@
 package org.uade.structure.implementation.fixed;
 
+import org.uade.exception.EmptyADTException;
+import org.uade.exception.FullADTException;
 import org.uade.structure.definition.QueueADT;
 
 public class StaticQueueADT implements QueueADT {
 
+    private static final int DEFAULT_CAPACITY = 1000;
+
+    private final int[] values;
+    private int head;
+    private int tail;
+    private int size;
+
+    public StaticQueueADT() {
+        this.values = new int[DEFAULT_CAPACITY];
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+
     @Override
     public int getElement() {
-        return 0;
+        if (this.isEmpty()) {
+            throw new EmptyADTException();
+        }
+
+        return this.values[this.head];
     }
 
     @Override
     public void add(int value) {
+        if (this.size == this.values.length) {
+            throw new FullADTException();
+        }
 
+        this.values[this.tail] = value;
+        this.tail = (this.tail + 1) % this.values.length;
+        this.size++;
     }
 
     @Override
     public void remove() {
+        if (this.isEmpty()) {
+            throw new EmptyADTException();
+        }
 
+        this.head = (this.head + 1) % this.values.length;
+        this.size--;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 }
